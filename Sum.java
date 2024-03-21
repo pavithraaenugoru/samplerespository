bfbb4b3fa8fe101b1caef5b79d76b3bced575665
pavithraaenugoru <raopavithra1@gmail.com>
2024-03-21 10:10:09 +0530

    my first commit

---
+++ Sum.java
@@ -0,0 +1,27 @@
+package com.basic;
+public class Sum {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int num,digit,sum=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the number:");
+		num=sc.nextInt();
+		while(num>0) {
+			digit=num%10;
+			sum=sum+digit;
+			num=num/10;
+		}
+		System.out.print("Sum of digits: "+sum);*/
+		int n=123;
+		//System.out.print((n/10)+(n%10)); //only for 2 digits
+		//System.out.print((n%10)+((n/10)%10)+(n/100)); //only for 3 digits
+		//System.out.print((n%4==0)?true:false); //leap yr using ternaryoperator
+		/*if(n==((n/100)+" "+(n%100)/10+" "+(n%100)%10)){
+			System.out.print("It is Palindrome");
+		}
+		else {
+			System.out.print("Not Palindrome");
+		}*/
+		System.out.print((n/100==(n%100)%10)?"p":"np");
+	}
+}
